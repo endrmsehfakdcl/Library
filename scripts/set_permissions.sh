3097dc0cc045630ed801cbba3509a41cee6efea5
endrmsehfakdcl <toyandy0205@gmail.com>
2023-12-10 01:12:56 +0900

    add script_dir

--- scripts/set_permissions.sh
+++ scripts/set_permissions.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-chmod +x /home/ec2-user/scripts/stop_server.sh
-chmod +x /home/ec2-user/scripts/start_server.sh
+
+SCRIPT_DIR="/home/ec2-user/scripts"
+
+if [ ! -d "$SCRIPT_DIR" ]; then
+    mkdir -p "$SCRIPT_DIR"
+fi
+
+chmod +x "$SCRIPT_DIR/stop_server.sh"
+chmod +x "$SCRIPT_DIR/start_server.sh"
