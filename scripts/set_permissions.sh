1b63edd3afb076df6be6b441234792d0b76aede6
endrmsehfakdcl <toyandy0205@gmail.com>
2023-12-10 01:48:35 +0900

    add appspec artifacts

--- scripts/set_permissions.sh
+++ scripts/set_permissions.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-SCRIPT_DIR="/home/ec2-user/scripts"
-
-if [ ! -d "$SCRIPT_DIR" ]; then
-    mkdir -p "$SCRIPT_DIR"
-fi
-
-chmod +x "$SCRIPT_DIR/stop_server.sh"
-chmod +x "$SCRIPT_DIR/start_server.sh"
+chmod +x /home/ec2-user/scripts/stop_server.sh
+chmod +x /home/ec2-user/scripts/start_server.sh
